177d29d4be87d00aee0531b392c872058c942e37
jt <jt@JT-Station.jtcima.com>
2022-09-05 18:59:59 +1000

    ${msg}

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from ast import parse
 from datetime import datetime
 import time
 import speech_recognition as sr
 import pyttsx3
 import webbrowser
 import wikipedia 
+import pywhatkit
 
 
 
@@ -19,33 +21,49 @@ def speak(text, rate = 178):
 
 def parseCommand():
     listener = sr.Recognizer()
-    listener.energy_threshold = 4000
+    
     print('Listening...')
 
     with sr.Microphone() as source:
-        listener.pause_threshold = 2
+        listener.pause_threshold = 1 #this is the pause time when you finish your command for ai to response
+        #listener.adjust_for_ambient_noise(source, duration=1)
+        #listener.dynamic_energy_threshold = True 
+        
         input_command = listener.listen(source)
     
-    try:
-        print('Recognizing command...')
-        query = listener.recognize_google(input_command, language='en_gb')
-        
-        print(f'Your command is: {query}')
-        speak(f'Your command is {query}')
-    except Exception as err:
-        print('Sorry, the command is unclear, please say again')
-        speak('Sorry, I did not get that, please say again')        
-        print(err)
-        return None
+        try:
+            print('Recognizing command...')
+            query = listener.recognize_google(input_command, language='en_gb')
+            print(f'Your command is: {query}')
+        except Exception as err:
+            #speak('Sorry, I did not get that, please say again')        
+            print(err)
+            query = 'please say again'
+    
     return query
 
 if __name__ == '__main__':
-    speak('system checkup complete, initialzing...')
+    speak('system checkup complete, initializing...')
     time.sleep(2)
-    speak('greetings')
+    speak('greetings all')
 
     while True:
         query = parseCommand().lower().split()
-
+        print(query)
+        if 'sima' in query or 'cima' in query or 'cosima' in query:
+            speak('What can I do for you master')
         #list commands
+        if 'open' in query and 'youtube' in query or 'youtube' in query:
+            webbrowser.open_new_tab('https://www.youtube.com')
+            speak('youtube is open')
+        if 'bye' in query or 'bye-bye' in query or 'cu' in query:
+            speak('system shutting down, see you master')
+            quit()
+        if 'time' in query:
+            time = datetime.now().strftime('%H:%M %p')
+            print(time)
+            speak('the current time is ' + time)
+            
+        
+        
 
